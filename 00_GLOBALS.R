21c1d1c8370b2ca0fc7b00868f246f105b7fff66
Andrija Djurovic <85941317+andrija-djurovic@users.noreply.github.com>
2021-07-08 12:28:52 +0200

    Delete 00_GLOBALS.R

--- 00_GLOBALS.R
+++
@@ -1,4 +0,0 @@
-#' @importFrom utils globalVariables
-utils::globalVariables(c("bin", "dist.b", "dist.g", "mod", "no", "pct.obs", "pct.y.sum",
-			 "sb", "sg", "so", "sy", "type", "woe", "x", "x.avg", 
-			 "x.max", "x.min", "y", "y.avg", "y.sum"))
